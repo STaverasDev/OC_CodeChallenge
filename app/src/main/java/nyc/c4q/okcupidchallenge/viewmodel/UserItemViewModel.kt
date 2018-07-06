1390774d956f1d55fadca98c3b3e5dda00d8dfaf
Shantal Taveras <shantaltaveras@ac.c4q.nyc>
2018-07-06 16:27:17 -0400

    create rec view user item view model

--- app/src/main/java/nyc/c4q/okcupidchallenge/viewmodel/UserItemViewModel.kt
+++ app/src/main/java/nyc/c4q/okcupidchallenge/viewmodel/UserItemViewModel.kt
@@ -1,13 +1,35 @@
 package nyc.c4q.okcupidchallenge.viewmodel
 
 import android.databinding.BaseObservable
+import android.databinding.Bindable
 import nyc.c4q.okcupidchallenge.model.KUser
 
 class UserItemViewModel(var user: KUser) : BaseObservable() {
 
+
+    @Bindable
     fun setNewUser(user: KUser) {
         this.user = user
     }
 
+    @Bindable
+    fun getUserName():String{
+        return user.userName
+    }
+
+    @Bindable
+    fun getAgeLocationForView():String{
+        return user.getAgeLocationForView()
+    }
+
+    @Bindable
+    fun getMatchforView():String{
+        return user.getAgeLocationForView()
+    }
+
+    @Bindable
+    fun getUserImg():String{
+        return user.photos.photoThumbnails.mediumThumbnail
+    }
 
 }
